c704827c246db4d51ffc790ad5bb81a0c0d8c1f7
Elihan303 <elihantejada@gmail.com>
2023-01-03 17:47:02 -0400

    first commit

---
+++ src/data/info.ts
@@ -0,0 +1,97 @@
+export const info = {
+  baseUrl: "https://astro-portfolio-uzair.vercel.app",
+  name: "Elihan Domingo Tejada Hilario",
+  jobDescription: "Software Engineer",
+  about: `Soy ingeniero de software y vivo en Santo Domingo, Republica Dominicana.
+   Me encanta la programacion. He podido acumular 2 años de experiencia en desarrollo de software.
+   Tengo 1 año de experiencia en tecnologias como Javascript, React con Typescript, C# .NET, SQL, SQL SERVER, entre otras tecnologias. Cuento con una certificacion en Javascript de parte la organizacion de FreeCodeCamp. Aun así, aprendo continuamente y nunca dejo de explorar nuevas tecnologías.Por lo tanto aun sigo aprendiendo nuevas tecnologias como Astro Build entre otras.
+  `,
+
+  experience: [
+    {
+      name: "Camsoft",
+      location: "Santo Domingo, Republica Dominicano",
+      startDate: "Feb. 2022",
+      endDate: "Presente",
+      description: [
+        "- Desarrollo de pantallas y componentes UI para la aplicaciónes web utilizando React,Typescript y DevExtreme.",
+        "- Desarrollo de API con C# .NET usado arquitectura Clean Code e inyecciones de dependencias.",
+        "- Manejo de bases de datos con SQL y SQL Server."
+      ],
+    }
+  ],
+
+  education: [
+    {
+      name: "Universidad Iberoamericana",
+      location: "Santo Domingo, Republica Dominicano",
+      startDate: "Ene. 2022",
+      endDate: "Presente",
+      description: [
+        "Ingeniería en Tecnologías de la Información y la Comunicación (TIC)",
+      ],
+    },
+    {
+      name: "Instituto Tecnológico de Las Américas",
+      location: "Santo Domingo, Republica Dominicano",
+      startDate: "Ene. 2020",
+      endDate: "Ago. 2022",
+      description: [
+        "Tecnologo en software",
+      ],
+    }
+  ],
+
+  socialMedia: {
+    facebook: "",
+    twitter: "https://twitter.com/elihan_th",
+    github: "https://github.com/Elihan303",
+    email: "elihantejada@gmail.com",
+    linkedin: "https://www.linkedin.com/in/elihan-th/",
+  },
+
+  projects: [
+    {
+      title: "Landing Page",
+      isFeatured: true,
+      thumbnail: "/assets/images/astroLanding.jpeg",
+      githubUrl: "https://github.com/Elihan303/Landing-page-astro",
+      liveUrl: "https://elihan303.github.io/Landing-page-astro/",
+    },
+    {
+      title: "Precios Crypto",
+      isFeatured: true,
+      thumbnail: "/assets/images/preciosCrypto.jpeg",
+      githubUrl: "https://github.com/Elihan303/Precios-crypto",
+      liveUrl: "https://precios-cryptos.netlify.app/",
+    },
+    {
+      title: "Task App",
+      isFeatured: true,
+      thumbnail: "/assets/images/task-app.jpeg",
+      githubUrl: "https://github.com/Elihan303/task-app-react",
+      liveUrl: "https://tasks-app-react.netlify.app/",
+    },
+    {
+      title: "Galeria de imagenes",
+      isFeatured: true,
+      thumbnail: "/assets/images/galeriaDeImagenes.jpeg",
+      githubUrl: "https://github.com/Elihan303/Galeria-de-imagenes",
+      liveUrl: "https://galeria-de-imagenes-de-elihan.netlify.app/",
+    },
+    {
+      title: "App de reconocimiento facil (Desktop)",
+      isFeatured: true,
+      thumbnail: "/assets/images/astro-portfolio.png",
+      githubUrl: "https://github.com/Elihan303/App_reconocimiento_facial",
+      liveUrl: "",
+    },
+    {
+      title: "Sistema de visista (Desktop)",
+      isFeatured: true,
+      thumbnail: "/assets/images/astro-portfolio.png",
+      githubUrl: "https://github.com/Elihan303/Sistema_Visitas",
+      liveUrl: "",
+    },
+  ],
+};
